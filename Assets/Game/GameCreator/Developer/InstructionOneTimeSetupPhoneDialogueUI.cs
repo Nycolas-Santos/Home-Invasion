5873162ac743af24ef5e84cfb4ce487b404ea84b
nycolas <nycolas.santos.workplace@outlook.com>
2023-03-09 01:24:46 -0300

    - Alterado escalonamento do canvas do Phone UI - Melhorado escala dos speeches de mensagens para encaixar na Tab de mensagens do Phone - Criado Setup de Phone Dialogue UI automatico via instrucao

---
+++ Assets/Game/GameCreator/Developer/InstructionOneTimeSetupPhoneDialogueUI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Game.Scripts.Phone;
+using GameCreator.Runtime.Common;
+using GameCreator.Runtime.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Object = UnityEngine.Object;
+
+[Version(1, 0, 0)]
+    
+[Title("Setup Phone Dialogue UI")]
+[Description("One time setup dialogue to support phone")]
+
+[Category("Custom/Setup Phone Dialogue UI")]
+
+[Keywords("Setup", "Phone", "Dialogue", "UI")]
+[Image(typeof(IconMobile), ColorTheme.Type.Blue)]
+
+[Serializable]
+public class InstructionOneTimeSetupPhoneDialogueUI : Instruction
+{
+    // MEMBERS: -------------------------------------------------------------------------------
+    [SerializeField] private PropertyGetGameObject m_DialogueUI = new PropertyGetGameObject();
+    
+    // PROPERTIES: ----------------------------------------------------------------------------
+
+    public override string Title => 
+        $"Setup Phone Dialogue UI";
+
+    // RUN METHOD: ----------------------------------------------------------------------------
+    
+    protected override Task Run(Args args)
+    {
+        var dialogueUI = m_DialogueUI.Get(args);
+        if (dialogueUI == null) return DefaultResult;
+
+        var phoneDialogueUI = Object.FindObjectOfType<PhoneDialogue>();
+        if (phoneDialogueUI == null) return DefaultResult;
+        
+        phoneDialogueUI.SetupPhoneUI(dialogueUI);
+        return DefaultResult;
+    }
+}
---
+++ Assets/Game/Scripts/Phone/PhoneDialogue.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Phone
+{
+    public class PhoneDialogue : MonoBehaviour
+    {
+        public void SetupPhoneUI(GameObject inventoryUI)
+        {
+        
+            var rectTransform = inventoryUI.GetComponent<RectTransform>();
+            if (rectTransform == null) return;
+        
+            rectTransform.SetParent(transform,false);
+            inventoryUI.transform.localPosition = Vector3.zero;
+
+            rectTransform.localScale = Vector3.one;
+            rectTransform.localRotation = Quaternion.identity;
+
+            rectTransform.offsetMin = Vector2.zero;
+            rectTransform.offsetMax = Vector2.zero;
+        
+            rectTransform.anchorMax = Vector2.one;
+            rectTransform.anchorMin = Vector2.zero;
+        }
+    }
+}
