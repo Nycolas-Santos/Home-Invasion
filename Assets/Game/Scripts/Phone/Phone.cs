e23d8bd301210bfcce27d150862a70e9434d64de
nycolas <nycolas.santos.workplace@outlook.com>
2023-03-28 02:42:38 -0300

    - Criado sistema para agilizar criacao de dialogos antigos na lista de contatos

--- Assets/Game/Scripts/Phone/Phone.cs
+++ Assets/Game/Scripts/Phone/Phone.cs
@@ -1,5 +1,6 @@
 using System;
 using GameCreator.Runtime.Common;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -7,13 +8,49 @@ namespace Game.Scripts.Phone
 {
     public class Phone : MonoBehaviour
     {
+        [BoxGroup("Core Phone Settings")]
         [SerializeField] private GameObject UI;
+        
+        [BoxGroup("Phone Message Settings")]
+        [SerializeField] private GameObject playerMessagePrefab;
+        [BoxGroup("Phone Message Settings")]
+        [SerializeField] private GameObject contactMessagePrefab;
+        [BoxGroup("Phone Message Settings")]
+        [SerializeField] private GameObject dateMessagePrefab;
+
+        
 
         private PhysicalPhone _physicalPhone;
 
+        private const string MISSING_MESSAGE_PREFAB = "MISSING MESSAGE PREFAB";
         private const string NO_PHYSICAL_PHONE_AVAILABLE = "There is no Physical Phone on the Player";
 
+        
+
+        #region Properties
+
         public static Phone Instance { get; set; }
+        
+        public GameObject PlayerMessagePrefab
+        {
+            get => playerMessagePrefab;
+            set => playerMessagePrefab = value;
+        }
+
+        public GameObject ContactMessagePrefab
+        {
+            get => contactMessagePrefab;
+            set => contactMessagePrefab = value;
+        }
+
+        public GameObject DateMessagePrefab
+        {
+            get => dateMessagePrefab;
+            set => dateMessagePrefab = value;
+        }
+
+        #endregion
+        
 
         private void Awake()
         {
@@ -23,8 +60,17 @@ namespace Game.Scripts.Phone
         private void Init()
         {
             Instance = this;
+            CheckReferences();
             //_physicalPhone = Object.FindObjectOfType<PhysicalPhone>(); DISABLED UNTIL REFACTOR
             //if (_physicalPhone == null) Debug.LogError(NO_PHYSICAL_PHONE_AVAILABLE); DISABLED UNTIL REFACTOR
+        }
+
+        private void CheckReferences()
+        {
+            if (playerMessagePrefab == null || contactMessagePrefab == null || dateMessagePrefab == null)
+            {
+                Debug.LogError(MISSING_MESSAGE_PREFAB);
+            }
         }
 
         public void CloseUI()
--- Assets/Game/Scripts/Phone/PhoneContact.cs
+++ Assets/Game/Scripts/Phone/PhoneContact.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using GameCreator.Runtime.Dialogue.UnityUI;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +10,25 @@ namespace Game.Scripts.Phone
     public class PhoneContact : MonoBehaviour
     {
         // MEMBERS
-        [SerializeField] private List<GameObject> messageLogs;
+        [BoxGroup("Contact Settings")]
+        [SerializeField] private string contactName;
+        [BoxGroup("Contact Settings")]
         [SerializeField] private Sprite contactPicture;
+        [BoxGroup("Contact Previous Messages")]
+        [SerializeField] private List<Message> messages;
+        [BoxGroup("Current Contact Messages")]
+        [SerializeField] private List<GameObject> messageLogs;
         
+
+        private Phone _phone;
         private PhoneMessageLogs _phoneMessageLogs;
         private PhoneMessageTab _phoneMessageTab;
         private PhoneCacheMessagesTab _phoneCacheMessagesTab;
         private PhoneCacheMessagesLogs _phoneCacheMessagesLogs;
 
         private Button _button;
+
+        private const string PLAYER_MESSAGE_NAME = "You";
         // PROPERTIES
         public List<GameObject> MessageLogs
         {
@@ -29,6 +41,25 @@ namespace Game.Scripts.Phone
             get => contactPicture;
             set => contactPicture = value;
         }
+        
+        [Serializable]
+        public struct Message
+        {
+            [ShowIf("type", Type.Message)]
+            public Sender sender;
+            public Type type;
+            [TextArea] public string content;
+            public enum Type
+            {
+                Message,
+                Date
+            }
+            public enum Sender
+            {
+                Player,
+                Contact
+            }
+        }
         // FUNCTIONS
 
         private void Awake()
@@ -37,6 +68,49 @@ namespace Game.Scripts.Phone
             if (_phoneMessageTab == null) _phoneMessageTab = FindObjectOfType<PhoneMessageTab>(true);
             if (_phoneCacheMessagesTab == null) _phoneCacheMessagesTab = FindObjectOfType<PhoneCacheMessagesTab>(true);
             if (_button == null) _button = GetComponent<Button>();
+            if (_phone == null) _phone = GetComponentInParent<Phone>(true);
+            if (messages.Count > 0) SetupContactMessages();
+        }
+
+        private void SetupContactMessages()
+        {
+            foreach (var message in messages)
+            {
+                GameObject newMessage = null;
+                Text contentText;
+                Text contactNameText;
+                switch (message.type)
+                {
+                    case Message.Type.Message:
+                        switch (message.sender)
+                        {
+                            case Message.Sender.Player:
+                                newMessage = Instantiate(_phone.PlayerMessagePrefab,transform.position,transform.rotation,transform);
+                                contactNameText = newMessage.GetComponentInChildren<PhoneMessageContactName>().GetComponent<Text>();
+                                contactNameText.text = PLAYER_MESSAGE_NAME;
+                                break;
+                            case Message.Sender.Contact:
+                                newMessage = Instantiate(_phone.ContactMessagePrefab,transform.position,transform.rotation,transform);
+                                contactNameText = newMessage.GetComponentInChildren<PhoneMessageContactName>().GetComponent<Text>();
+                                contactNameText.text = contactName;
+                                break;
+                        }
+                        contentText = newMessage.GetComponentInChildren<PhoneMessageText>().GetComponent<Text>();
+
+                        contentText.text = message.content;
+                        
+                        break;
+                    case Message.Type.Date:
+                        newMessage = Instantiate(_phone.DateMessagePrefab,transform.position,transform.rotation,transform);
+                        
+                        contentText = newMessage.GetComponentInChildren<PhoneMessageText>().GetComponent<Text>();
+
+                        contentText.text = message.content;
+                        break;
+                }
+                newMessage.SetActive(false);
+                messageLogs.Add(newMessage);
+            }
         }
 
         private void OnEnable()
